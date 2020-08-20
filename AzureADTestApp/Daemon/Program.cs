b316062dae7fbf4f0a8821dffc7a9c1f84e0e21b
Ryan Dockstader <ryanldockstader@gmail.com>
2020-08-20 16:54:42 -0600

    move to remote repo

---
+++ AzureADTestApp/Daemon/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace AzureADTestApp.Daemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making the call, fingers crossed...");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json");
+            var authResult = await Authenticate(config);
+
+            if(!string.IsNullOrEmpty(authResult.AccessToken))
+            {
+                Console.WriteLine();
+                Console.WriteLine("----------------");
+                Console.WriteLine();
+                await RequestWeatherData(config, authResult);
+            }
+        }
+
+        private static async Task RequestWeatherData(AuthConfig config, AuthenticationResult authResult)
+        {
+            var httpClient = new HttpClient();
+            var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+            if (defaultRequestHeaders.Accept == null ||
+                !defaultRequestHeaders.Accept.Any(m => m.MediaType == "applicatin/json"))
+            {
+                httpClient.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            defaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+            HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                string json = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(json);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to call API: {response.StatusCode}");
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Content: {content}");
+            }
+            Console.ResetColor();
+        }
+
+        private static async Task<AuthenticationResult> Authenticate(AuthConfig config)
+        {
+   
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            string[] ResourceIds = new string[] { config.ResourceId };
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token Aquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+
+            }
+
+            return result;
+        }
+    }
+}
